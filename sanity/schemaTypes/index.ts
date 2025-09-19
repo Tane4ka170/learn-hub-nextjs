23ac00f40c6a5b63097b800494082c9b9229bef0
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-19 19:45:30 +0300

    feat: Registered all core schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,33 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { studentType } from "./studentType";
+import { courseType } from "./courseType";
+import { instructorType } from "./instructorType";
+import { moduleType } from "./moduleType";
+import { lessonType } from "./lessonType";
+import { categoryType } from "./categoryType";
+import { blockContent } from "./blockContent";
+import { enrollmentType } from "./enrollmentType";
+import { lessonCompletionType } from "./lessonCompletionType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [studentType],
+  types: [
+    studentType,
+    courseType,
+    instructorType,
+    moduleType,
+    lessonType,
+    categoryType,
+    blockContent,
+    enrollmentType,
+    lessonCompletionType,
+  ],
 };
 
 export * from "./studentType";
+export * from "./courseType";
+export * from "./instructorType";
+export * from "./moduleType";
+export * from "./lessonType";
+export * from "./categoryType";
+export * from "./enrollmentType";
+export * from "./lessonCompletionType";
