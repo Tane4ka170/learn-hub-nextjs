5636a4bd373cf2b19a7374634e5d1e0136c13b3e
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-22 17:12:59 +0300

    config: Setup live content API with tokens

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -2,8 +2,17 @@
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
 import { defineLive } from "next-sanity/live";
-import { client } from './client'
+import { client } from "./client";
+
+const token = process.env.NEXT_PUBLIC_SANITY_API_TOKEN;
+if (!token) {
+  throw new Error("NEXT_PUBLIC_SANITY_API_TOKEN is not set");
+}
 
 export const { sanityFetch, SanityLive } = defineLive({
-  client,
+  client: client.withConfig({
+    apiVersion: "vX",
+  }),
+  serverToken: token,
+  browserToken: token,
 });
