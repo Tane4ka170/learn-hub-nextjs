21f7ff0d682f2bb8a0158c69ee4ebef7b3cd7eab
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-23 17:08:38 +0300

    refactor: Move search page to dynamic route

--- app/(user)/search/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-async function SearchPage(props: { searchParams: { query: string } }) {
-const result=await({await searchParams}.query)
-  return <div>SearchPage</div>;
-}
-
-export default SearchPage;
