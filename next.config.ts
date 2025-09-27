033521a9d36425096769d059a3b9924acb0cf773
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-27 19:10:30 +0300

    feat: Allow images from Clerk domain

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**",
       },
+      {
+        hostname: "img.clerk.com",
+        protocol: "https",
+      },
     ],
   },
 };
